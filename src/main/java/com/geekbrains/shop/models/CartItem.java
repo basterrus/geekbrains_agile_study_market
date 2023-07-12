0d4bc13e1e3abd21adafbc040d634e392438e3ba
baster <pnedoshivin@gmail.com>
2023-07-12 15:10:36 +0300

    issue -7 init

---
+++ src/main/java/com/geekbrains/shop/models/CartItem.java
@@ -0,0 +1,26 @@
+package com.geekbrains.shop.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CartItem {
+    private Long productId;
+    private String productTitle;
+    private int quantity;
+    private BigDecimal pricePerProduct;
+    private BigDecimal price;
+    private String image;
+
+    public void changeQuantity(int delta) {
+        quantity += delta;
+        price = pricePerProduct.multiply(BigDecimal.valueOf(quantity)).setScale(2, RoundingMode.HALF_UP);
+    }
+
+}
