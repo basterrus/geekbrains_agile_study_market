df85005ca8488e0397e7a537fccb59059418b257
Sergey <elec84@mail.ru>
2023-07-30 17:24:55 +0300

    add auth_form

---
+++ src/main/resources/static/auth.js
@@ -0,0 +1,8 @@
+angular.module('market', []).controller('indexController', function ($scope, $http) {
+
+    $scope.login = function () {
+        $http.post('http://localhost:8888/api/v1/auth/login', $scope.user).then(function (response) {
+            $scope.userToken = response.data;
+        });
+    }
+});
